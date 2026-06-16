6b6903330403f2884242867b68b1f790cddcfdf2
EC2 Default User <ec2-user@ip-172-31-47-179.ap-south-1.compute.internal>
2026-06-16 10:12:04 +0000

    Updated app message for pipeline test

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-version = os.getenv("VERSION", "BLUE")
-
 @app.route("/")
 def home():
-    return f"Blue-Green App Running - {version}"
+    return f" just Blue-Green App Running - {os.getenv('ENV', 'BLUE')}"
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
